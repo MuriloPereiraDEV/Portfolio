4734bb72d64f6283184266f90b8591a3005642c6
Murilo Pereira <murilo.pereira@softex.br>
2022-10-23 22:58:26 -0300

    upload project

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def portfolio():
-    return render_template("main.html")
+    return render_template("template/main.html")
 
 
 if __name__ == "__main__":
